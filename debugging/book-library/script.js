f1bcc2aec41c70315b667da55544c164468a5966
cywong <zenobiatony@gmail.com>
2026-08-07 17:59:15 +0100

    Refactor book creation and storage in populateStorage
    Remove newline characters;
    Change "127" to 127 ;
    Simplify two push statement to one push statement

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -8,14 +8,8 @@ window.addEventListener("load", function (e) {
 function populateStorage() {
   if (myLibrary.length == 0) {
     let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
-    let book2 = new Book(
-      "The Old Man and the Sea",
-      "Ernest Hemingway",
-      "127",
-      true
-    );
-    myLibrary.push(book1);
-    myLibrary.push(book2);
+    let book2 = new Book("The Old Man and the Sea","Ernest Hemingway",127,true);
+    myLibrary.push(book1, book2);
     render();
   }
 }
