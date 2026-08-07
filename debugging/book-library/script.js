d620ffbb48d9c243ba380ea16ce3790253d2f4f8
cywong <zenobiatony@gmail.com>
2026-08-07 18:16:20 +0100

    Simplify rendering of library rows and buttons
    Refactor loop to directly use myLibrary.length and simplify read status button logic.

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -52,35 +52,30 @@ function render() {
     table.deleteRow(1);
   }
   //insert updated row and cells
-  let length = myLibrary.length;
-  for (let i = 0; i < length; i++) {
+  
+  for (let i = 0; i < myLibrary.length; i++) {
     let row = table.insertRow(-1);
+  
     let titleCell = row.insertCell(0);
     let authorCell = row.insertCell(1);
     let pagesCell = row.insertCell(2);
     let wasReadCell = row.insertCell(3);
     let deleteCell = row.insertCell(4);
+    
     titleCell.innerHTML = myLibrary[i].title;
     authorCell.innerHTML = myLibrary[i].author;
     pagesCell.innerHTML = myLibrary[i].pages;
 
     //add and wait for action for read/unread button
     let changeBut = document.createElement("button");
-    changeBut.id = i;
+    
     changeBut.className = "btn btn-success";
-    wasReadCell.appendChild(changeBut);
-    let readStatus = "";
-    if (myLibrary[i].check == true) {
-      readStatus = "Yes";
-    } else {
-      readStatus = "No";
-    }
-    changeBut.innerText = readStatus;
-
-    changeBut.addEventListener("click", function () {
+    changeBut.innerText = myLibrary[i].check ? "Yes" : "No";
+    changeBut.addEventListener("click", () => {
       myLibrary[i].check = !myLibrary[i].check;
       render();
     });
+    wasReadCell.appendChild(changeBut);
 
     //add delete button to every row and render again
     let delButton = document.createElement("button");
