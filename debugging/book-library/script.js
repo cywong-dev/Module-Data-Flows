ba9ac926300470c667dc08a7cabc35e27b1d32cc
cywong <zenobiatony@gmail.com>
2026-08-12 14:29:03 +0100

    Change innerHTML to textContent for safety

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -119,9 +119,9 @@ function render() {
     let wasReadCell = row.insertCell(3);
     let deleteCell = row.insertCell(4);
     
-    titleCell.innerHTML = myLibrary[i].title;
-    authorCell.innerHTML = myLibrary[i].author;
-    pagesCell.innerHTML = myLibrary[i].pages;
+    titleCell.textContent = myLibrary[i].title;
+    authorCell.textContent = myLibrary[i].author;
+    pagesCell.textContent = myLibrary[i].pages;
 
     //add and wait for action for read/unread button
     let changeBut = document.createElement("button");
